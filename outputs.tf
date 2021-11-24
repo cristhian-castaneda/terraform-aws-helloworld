954c90dd003c57c19213bd97c7922fdacee50b9e
Cristhian Castaneda <ccastanedarivera@gmail.com>
2021-11-23 19:59:18 -0600

    feat(output): add output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "my_password" {
   value     = var.password
   sensitive = true
 }
+
+output "my_custom_output" {
+  value     = "I want to see within spinnaker pipeline context"
+  sensitive = false
+}
